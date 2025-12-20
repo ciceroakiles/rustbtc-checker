c09978cbd0124d4fc23a9516e35ad20d2c61b3a6
ciceroakiles <cicero.akiles@gmail.com>
2025-12-20 04:44:56 -0300

    chore: display sort dataframe, update gitignore

--- jsonl_analysis.py
+++ jsonl_analysis.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
+# Display all lines
 #pd.set_option('display.max_rows', None)
 
 
 def main():
+    # Build dataframe
     filepath = "jsonl/rust-bitcoin-addresses-src-lib.rs.jsonl"
     df = pd.read_json(filepath, lines=True)
     df = df.sort_values(by='crate_id', ascending=True)
-    print(df)
+
+    # Show dataframe
+    print("Dataframe for:", filepath[6:], "\n", df)
 
 
 if __name__ == "__main__":
